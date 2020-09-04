979188e36e9006308113019e5cb327bf4deabac7
Dennis Lee <leedennis04@gmail.com>
2020-09-04 10:34:23 -0500

    first commit

---
+++ src/test/java/com/dennislee/devsondeck/DevsOnDeckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dennislee.devsondeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsOnDeckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
